426ab964af10716b3d34f824e7c89605621ec349
kanaru-ssk <2019january.0131@gmail.com>
2021-12-28 18:40:03 +0900

    調整

--- public/js/456.main.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunklistapp=self.webpackChunklistapp||[]).push([[456],{456:(e,t,s)=>{s.r(t),s.d(t,{showHowpage:()=>n});const n=()=>{const e=new XMLHttpRequest;e.open("GET","/template/how.html",!0),e.onload=()=>{if(e.status>=200&&e.status<400){const t=e.responseText;document.getElementById("main").innerHTML=t}},e.send()}}}]);
